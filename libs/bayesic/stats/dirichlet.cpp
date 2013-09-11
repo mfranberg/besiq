f188687bbdf35efa82b3cf9a464701aea74a9eba
Mattias Franberg <mattias.franberg@gmail.com>
2013-09-11 11:48:36 +0200

    Added support for sampling dirichlet random numbers. Put lbinomial in the same header for now.

--- libs/bayesic/stats/dirichlet.cpp
+++ libs/bayesic/stats/dirichlet.cpp
@@ -1,6 +1,7 @@
 #include <assert.h>
 
 #include <stats/dirichlet.hpp>
+#include <libdcdf.hpp>
 
 double
 dirmult(const arma::vec &x, const arma::vec &alpha)
@@ -25,4 +26,32 @@ ldirmult(const arma::vec &x, const arma::vec &alpha)
     }
 
     return K + x_alpha;
+}
+
+double
+lbinomial(double n, double k)
+{
+    assert( k > 0.0 );
+    assert( n >= k );
+
+    return lgamma( n + 1.0 ) - lgamma( n - k + 1.0 ) - lgamma( k + 1.0 );
+}
+
+dir_generator::dir_generator(unsigned long seed)
+    :   m_generator( seed )
+{
+
+}
+
+arma::vec
+dir_generator::sample(const arma::vec &alpha)
+{
+    arma::vec x = arma::zeros<arma::vec>( alpha.n_elem );
+    for(int i = 0; i < alpha.n_elem; i++)
+    {
+        double runif =((double) m_generator( )) / ( m_generator.max( ) - m_generator.min( ) );
+        x[ i ] = gamma_cdf_inv( runif, alpha[ i ], 1.0 );
+    }
+
+    return x / sum( x );
 }
