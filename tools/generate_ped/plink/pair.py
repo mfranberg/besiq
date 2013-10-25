308535e17aff4a9d1f49db7da8f3cf9afd58a032
Mattias Franberg <mattias.franberg@gmail.com>
2013-10-25 13:40:41 +0200

    Added pair file.

--- tools/generate_ped/plink/pair.py
+++ tools/generate_ped/plink/pair.py
@@ -9,16 +9,14 @@ class PairFile:
     #
     def __init__(self, path):
         self.file = open( path, "w" )
+        self.index = 1
 
     ##
-    # Writes a pair with the given indicies ot
-    # the file.
+    # Writes a pair with the next index.
     #
-    # @param snp1 Index of snp1.
-    # @param snp2 Index of snp2.
-    #
-    def write(self, snp1, snp2):
-        self.file.write( "snp{0} snp{1}\n".format( snp1, snp2 ) )
+    def write(self, ):
+        self.file.write( "snp{0} snp{1}\n".format( self.index, self.index + 1 ) )
+        self.index += 2
 
     ##
     # Closes the file.
