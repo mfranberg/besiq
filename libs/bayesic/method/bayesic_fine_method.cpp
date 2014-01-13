bd0c0660f4a70575e724a2665a355d4d0384d699
Mattias Franberg <mattias.franberg@gmail.com>
2014-01-13 14:23:01 +0100

    Changed fine method to support single argument.

--- libs/bayesic/method/bayesic_fine_method.cpp
+++ libs/bayesic/method/bayesic_fine_method.cpp
@@ -3,11 +3,16 @@
 bayesic_fine_method::bayesic_fine_method(method_data_ptr data, int num_mc_iterations, arma::vec alpha)
     : bayesic_method::bayesic_method( data )
 {
-    double prior = 1.0 / ( 2.0 * data->num_interactions );
+    double int_prior = 1.0 / ( 2.0 * data->num_interactions );
+    
+    if( data->single_prior > 0.0 )
+    {
+        int_prior = data->single_prior;
+    }
 
     std::vector<model *> models;
-    models.push_back( new saturated( prior, alpha ) );
-    models.push_back( new sindependent( 1.0 - prior, alpha, num_mc_iterations ) );
+    models.push_back( new saturated( int_prior, alpha ) );
+    models.push_back( new sindependent( 1.0 - int_prior, alpha, num_mc_iterations ) );
 
     bayesic_method::set_models( models );
 }
