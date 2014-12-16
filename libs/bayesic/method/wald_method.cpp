e10e3803ac5a8403838d888c6d324713bcc04990
Mattias Frånberg <mattias.franberg@gmail.com>
2014-12-16 16:06:12 +0100

    Implemented the Wald test for testing interactions.

---
+++ libs/bayesic/method/wald_method.cpp
@@ -0,0 +1,98 @@
+#include <dcdflib/libdcdf.hpp>
+#include <bayesic/method/wald_method.hpp>
+#include <bayesic/stats/snp_count.hpp>
+
+wald_method::wald_method(method_data_ptr data)
+: method_type::method_type( data )
+{
+    m_weight = arma::ones<arma::vec>( data->phenotype.n_elem );
+}
+
+void
+wald_method::init(std::ostream &output)
+{
+    output << "LR\tP";
+}
+
+void
+wald_method::run(const snp_row &row1, const snp_row &row2, std::ostream &output)
+{
+    arma::mat n = joint_count( row1, row2, get_data( )->phenotype, m_weight );
+    if( arma::min( arma::min( n ) ) <= 10 )
+    {
+        output << "NA\tNA";
+    }
+
+    arma::vec log_or0( 4 );
+    arma::vec log_or1( 4 );
+    arma::mat I0( 4, 4 );
+    arma::mat I1( 4, 4 );
+    arma::mat I( 4, 4 );
+    arma::mat Iinv( 4, 4 );
+    arma::vec log_diff( 4 );
+    int elem = 0;
+    for(int i = 1; i <= 2; i++)
+    {
+        for(int j = 1; j <= 2; j++)
+        {
+            
+            log_or0[ elem ] = log( n( i*3 + j, 0 ) * n( 0*3 + 0, 0 ) / n( 0*3 + j, 0 ) / n( i*3 + 0, 0 ) );
+            log_or1[ elem ] = log( n( i*3 + j, 1 ) * n( 0*3 + 0, 1 ) / n( 0*3 + j, 1 ) / n( i*3 + 0, 1 ) );
+            I0( elem, elem ) = 1.0 / n( i*3 + j, 0 ) + 1.0 / n( 0*3 + 0, 0 ) + 1.0 / n( 0*3 + j, 0 ) + 1.0 / n( i*3 + 0, 0 );
+            I1( elem, elem ) = 1.0 / n( i*3 + j, 1 ) + 1.0 / n( 0*3 + 0, 1 ) + 1.0 / n( 0*3 + j, 1 ) + 1.0 / n( i*3 + 0, 1 );
+
+            elem++;
+        }
+    }
+
+    I0(0, 1) = 1.0 / n( 0*3 + 0, 0 ) + 1.0 / n( 1*3 + 0, 0 );
+    I1(0, 1) = 1.0 / n( 0*3 + 0, 1 ) + 1.0 / n( 1*3 + 0, 1 );
+
+    I0(0, 2) = 1.0 / n( 0*3 + 0, 0 ) + 1.0 / n( 0*3 + 1, 0 );
+    I1(0, 2) = 1.0 / n( 0*3 + 0, 1 ) + 1.0 / n( 0*3 + 1, 1 ); 
+
+    I0(0, 3) = 1.0 / n( 0*3 + 0, 0 );
+    I1(0, 3) = 1.0 / n( 0*3 + 0, 1 ); 
+    
+    I0(1, 2) = I0(0, 3); 
+    I1(1, 2) = I1(0, 3); 
+
+    I0(1, 3) = 1.0 / n( 0*3 + 0, 0 ) + 1.0 / n( 0*3 + 2, 0 );
+    I1(1, 3) = 1.0 / n( 0*3 + 0, 1 ) + 1.0 / n( 0*3 + 2, 1 ); 
+
+    I0(2, 3) = 1.0 / n( 0*3 + 0, 0 ) + 1.0 / n( 2*3 + 0, 0 );
+    I1(2, 3) = 1.0 / n( 0*3 + 0, 1 ) + 1.0 / n( 2*3 + 0, 1 );
+
+    log_diff = log_or0 - log_or1;
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = i; j < 4; j++)
+        {
+            I( i, j ) = I0( i, j ) + I1( i, j );
+        }
+    }
+    
+    for(int i = 1; i < 4; i++)
+    {
+        for(int j = 0; j < i; j++)
+        {
+            I( i, j ) = I( j, i );
+        }
+    }
+
+    if( !inv( Iinv, I ) )
+    {
+        output << "NA\tNA";
+    }
+    
+    double chi = 0.0;
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = 0; j < 4; j++)
+        {
+            chi += Iinv( i , j ) * log_diff[ i ] * log_diff[ j ];
+        }
+    }
+
+    output << chi << "\t" << 1.0 - chi_square_cdf( chi, 4 );
+}
---
+++ libs/bayesic/method/wald_method.hpp
@@ -0,0 +1,44 @@
+#ifndef __WALD_METHOD_H__
+#define __WALD_METHOD_H__
+
+#include <string>
+#include <vector>
+
+#include <armadillo>
+
+#include <bayesic/method/method.hpp>
+#include <bayesic/stats/log_scale.hpp>
+
+/**
+ * This class is responsible for executing the closed form
+ * wald test for a logistic regression model.
+ */
+class wald_method
+: public method_type
+{
+public:
+    /**
+     * Constructor.
+     *
+     * @param data Additional data required by all methods, such as
+     *             covariates.
+     */
+    wald_method(method_data_ptr data);
+    
+    /**
+     * @see method_type::init.
+     */
+    virtual void init(std::ostream &output);
+    
+    /**
+     * @see method_type::run.
+     */
+    virtual void run(const snp_row &row1, const snp_row &row2, std::ostream &output);
+private:
+    /**
+     * Weight for each sample.
+     */
+    arma::vec m_weight;
+};
+
+#endif /* End of __WALD_METHOD_H__ */
