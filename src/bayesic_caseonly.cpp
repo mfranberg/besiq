7d86acacd3b5168d3cbe4cb589c79bd9f1cb24c0
Mattias Franberg <mattias.franberg@gmail.com>
2015-05-04 14:27:00 +0200

    Implemented Peer's screening test and contrast test.

--- src/bayesic_caseonly.cpp
+++ src/bayesic_caseonly.cpp
@@ -5,6 +5,7 @@
 #include <cpp-argparse/OptionParser.h>
 
 #include <bayesic/method/caseonly_method.hpp>
+#include <bayesic/method/peer_method.hpp>
 #include <bayesic/method/method.hpp>
 
 #include "common_options.hpp"
@@ -18,7 +19,7 @@ const std::string DESCRIPTION = "A stage-wise case-only filter for genetic inter
 int
 main(int argc, char *argv[])
 {
-    char const* const method_choices[] = { "css", "r2", "contrast" };
+    char const* const method_choices[] = { "css", "r2", "contrast", "peer" };
     OptionParser parser = create_common_options( USAGE, DESCRIPTION, false );
     parser.add_option( "-m", "--method" ).choices( &method_choices[ 0 ], &method_choices[ 3 ] ).metavar( "method" ).help( "The type of test statistic to compute 'r2' or 'css'." ).set_default( "css" );
     
@@ -30,7 +31,15 @@ main(int argc, char *argv[])
     }
     shared_ptr<common_options> parsed_data = parse_common_options( options, parser.args( ) );
 
-    method_type *m = new caseonly_method( parsed_data->data, options[ "method" ] );
+    method_type *m = NULL;
+    if( options[ "method" ] != "peer" )
+    {
+        m = new caseonly_method( parsed_data->data, options[ "method" ] );
+    }
+    else if( options[ "method" ] == "peer" )
+    {
+        m = new peer_method( parsed_data->data );
+    }
     
     run_method( *m, parsed_data->genotypes, *parsed_data->pairs, *parsed_data->result_file );
 
